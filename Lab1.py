8f2f7f5d5634a77173d1a94289cda2d34b083950
MatBar29 <92025651+MatBar29@users.noreply.github.com>
2022-10-06 09:27:50 +0200

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,29 @@
+#1
+a = "J"
+b = "Kowalski"
+
+print(a+"."+b)
+#2
+a2 = "jan"
+b2 = "kowalski"
+print(a2[0].capitalize()+"."+b2.capitalize())
+#3
+pierwsze = 20
+ostatnie = 22
+wiek = 32
+
+if wiek>ostatnie:
+    rok=(pierwsze*100)-(wiek-ostatnie)
+    print(rok)
+elif ostatnie == wiek:
+    print(pierwsze*100)
+else:
+    print(pierwsze*100+(ostatnie-wiek))
+#5
+a3 = 28
+b3 = 7
+
+if a3 > 0 and b3 > 0:
+    print(a3/b3)
+#6
+
